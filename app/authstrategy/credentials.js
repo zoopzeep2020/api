5ff7a9a6dc85f78ab52192b02293699688fda19c
makasana <makasanas@yahoo.in>
2018-07-16 10:28:32 +0530

    login issue updated

--- app/authstrategy/credentials.js
+++ app/authstrategy/credentials.js
@@ -17,7 +17,7 @@ class CredentialsAuthStrategy extends LocalAuthStrategy {
     }
 
     static handleUserAuth(username, password, done) {
-        UserModel.findOne({email: username.toLowerCase()}).populate({ path: 'storeId', populate:[{ path: 'categoriesIds', model: 'Category', select: ['category', 'categoryImage', 'categoryActiveImage']} , { path: 'keyword', model: 'Keyword', select: ['title'] } , { path: 'storeCatalogs', model: 'Catalog', select: ['catalogUrl', 'catalogDescription'] } ],  model: 'Store'  }).exec(function(err, user) {
+        UserModel.findOne({ email: username.toLowerCase() }).populate({ path: 'storeId', populate: [{ path: 'categoriesIds', model: 'Category', select: ['category', 'categoryImage', 'categoryActiveImage'] }, { path: 'storeCatalogs', model: 'Catalog', select: ['catalogUrl', 'catalogDescription'] }], model: 'Store' }).exec(function (err, user) {
             if (err) {
                 return done(err);
             }
@@ -31,7 +31,7 @@ class CredentialsAuthStrategy extends LocalAuthStrategy {
         })
 
         // UserModel.findOne({email: username}, function (err, user) {
-            
+
         // });
     }
 
