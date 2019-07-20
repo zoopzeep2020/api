b96985c8b1611977dbd32f58ef3ee5c004b6a8d1
zoopzeep2020 <53048207+zoopzeep2020@users.noreply.github.com>
2019-07-20 15:59:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -140,6 +140,174 @@ app.get('/swagger', function (req, res) {
 
 //     scope: ['public_profile', 'email']
 // }));
+
+app.post('/save', function(req,res){
+	let data = req.body;	
+	getWallet(data.user_id).then(function(resp){
+		//main amount minus wallet amout
+			let wallet_minus_amount = (Math.round(resp.wallet_total_amount * data.cashback_add_to_wallet_percentage) / 100);
+			let obj = {
+				"amount" : data.amount,
+				"user_id" : data.user_id,
+				"cashback_deduction_percentage" : data.cashback_deduction_percentage,
+				"cashback_add_to_wallet_percentage" : data.cashback_add_to_wallet_percentage,
+				"cashback_amount" : wallet_minus_amount,
+				"description" : (wallet_minus_amount > 0) ? "Cashback Amount" : "Initial Amount",
+				"flag": false 
+			}
+			//deduction amount save into database
+			saveWallet(obj).then(function(walletResp){
+				console.log("deduction amount save into wallet : ",walletResp);
+				let paid_amout = (data.amount - wallet_minus_amount);
+				let user_cashback_amount = (Math.round(paid_amout * data.cashback_deduction_percentage) /100);
+				
+				// record store into database
+				saveProduct(data).then(function(productResp){
+					console.log("called save method",productResp);
+					
+					var newCashbackObj = {
+						"amount" : data.amount,
+						"paid_amount" : paid_amout,
+						"user_id" : data.user_id,
+						"cashback_deduction_percentage" : data.cashback_deduction_percentage,
+						"cashback_add_to_wallet_percentage" : data.cashback_add_to_wallet_percentage,
+						"cashback_amount" : user_cashback_amount,
+						"description" : "cashback Amount",
+						"flag": true 
+					}
+					//new cashback amount save into database
+					saveWallet(newCashbackObj).then(function(resp){
+                        // res.send(resp);
+                        var respData = {
+                            data : productResp,
+                            status:1,
+                            message : "Successful!"
+                        }
+                        res.send(respData);
+					}).catch(function(err){
+						console.log("save product error",err);
+						res.status(403).send(err);
+					});
+				}).catch(function(err){
+					console.log("save product error",err);
+					res.status(403).send(err);
+				});
+
+			}).catch(function(err){
+				console.log("save wallet error",err);
+				res.status(403).send(err);
+			});
+	});
+});
+app.post('/wallet', function(req,res){
+	let data = req.body;	
+	saveWallet(data).then(function(resp){
+        console.log("called save method",resp);
+        var respData = {
+            data : resp,
+            status:1,
+            message : "Successful!"
+        }
+		res.send(respData);
+	}).catch(function(err){
+		console.log("save wallet error",err);
+		res.status(403).send(err);
+	});
+
+});
+
+app.post('/getwallet', function(req,res){
+	let data = req.body;	
+	getWallet(data.user_id).then(function(resp){
+        console.log("called save method",resp);
+        res.send(resp);
+	})
+
+});
+
+function saveProduct(data){
+	return new Promise((resolve, reject) => {
+		
+		const product = new ProductModel(data);
+
+		product.save()
+        .then(data => {
+            resolve(data);
+        }).catch(err => {
+            reject(err);
+        });
+	});
+}
+
+function saveWallet(data){
+	return new Promise((resolve, reject) => {
+		
+		const wallet = new WalletModel(data);
+
+		wallet.save()
+        .then(data => {
+            resolve(data);
+        }).catch(err => {
+            reject(err);
+        });
+	});
+}
+
+function getWallet(userId){
+	return new Promise((resolve, reject) => {
+		WalletModel.find({user_id : userId}).then(function(data){
+			if(data.length > 0){
+				let wallet_total_amount = 0;
+				for(var i = 0; i < data.length; i ++){
+					if(data[i].flag == true){
+						wallet_total_amount = wallet_total_amount + data[i].cashback_amount;
+					}
+					if(data[i].flag == false){
+						wallet_total_amount = wallet_total_amount - data[i].cashback_amount;
+					}
+				}
+				var dataObj = {
+					data: data,
+					wallet_total_amount: wallet_total_amount
+				}
+				resolve(dataObj);
+			}else{
+				var dataObj = {
+					data: [],
+					wallet_total_amount: 0
+				}
+				resolve(dataObj);
+			}
+			
+		}).catch(err => {
+            reject(err);
+        });
+	});
+}
+
+/* Nirav Raval Code Here 12-07-2019 END*/
+
+function GetWalletBalance(data){
+    console.log(data);
+    new Promise(function(resolve, reject){
+    WalletCalculation.find({})
+    .then((item)=>{
+        console.log("get result",item);
+        resolve(item);
+    })
+    .catch((err)=>{
+    console.log(err);
+    reject(err);
+    })
+});
+
+
+}
+
+
+
+
+
 var http = require('http');
 var serverExpress = http.createServer(app)
 app.use(express.static(__dirname + '/swagger'));
